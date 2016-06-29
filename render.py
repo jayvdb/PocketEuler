1dae91a5d23713c34e868d4e059bb8721a30e6c0
John Vandenberg <jayvdb@gmail.com>
2016-06-30 00:15:07 +0700

    Prototype rendering equations

---
+++ render.py
@@ -0,0 +1,35 @@
+import codecs
+
+from bs4 import BeautifulSoup
+
+import sarge
+
+
+def load_raw():
+    f = codecs.open('README.md', 'r', 'utf8')
+    return f
+
+
+def extract_equations(raw):
+    soup = BeautifulSoup(raw, 'lxml')
+    equation_tags = soup.find_all('img', class_="equation")
+    equations = dict(
+        (item['src'], item['data-expr'])
+        for item in equation_tags
+    )
+    return equations
+
+
+def create_svg(filename, maths):
+    sarge.run("tex2svg '{0}' > {1}".format(maths, filename))
+
+
+def main():
+    f = load_raw()
+    equations = extract_equations(f)
+    for filename, maths in equations.items():
+        create_svg(filename, maths)
+
+
+if __name__ == '__main__':
+    main()
